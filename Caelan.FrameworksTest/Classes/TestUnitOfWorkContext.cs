d72953cb32d50bf56f594ec72ae918141c631c66
Matteo Bruni <matteo.bruni@me.com>
2014-07-02 01:42:18 +0200

    fix for c# project

--- Caelan.FrameworksTest/Classes/TestUnitOfWorkContext.cs
+++ Caelan.FrameworksTest/Classes/TestUnitOfWorkContext.cs
@@ -6,10 +6,9 @@ namespace Caelan.FrameworksTest.Classes
 {
     public class TestUnitOfWorkContext : IUnitOfWork
     {
-        private TestDbContext _context;
         public DbContext Context()
         {
-            return _context ?? (_context = new TestDbContext());
+            return new TestDbContext();
         }
     }
 }
