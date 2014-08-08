9f098bf28a26e8500740f8f5bf1c196684ed6b36
Matteo Bruni <matteo.bruni@me.com>
2014-08-08 14:11:36 +0200

    Fixed with packages updates

--- Caelan.FrameworksTest/Classes/UserRepository.cs
+++ Caelan.FrameworksTest/Classes/UserRepository.cs
@@ -4,27 +4,26 @@ using System.Linq.Expressions;
 using Caelan.Frameworks.BIZ.Classes;
 using Caelan.Frameworks.Common.Extenders;
 using Caelan.FrameworksTest.Models;
-using Microsoft.FSharp.Core;
 
 namespace Caelan.FrameworksTest.Classes
 {
-	public class UserRepository : BaseCRUDRepository<User, UserDTO>
-	{
-		public UserRepository(BaseUnitOfWork manager)
-			: base(manager)
-		{
-		}
+    public class UserRepository : BaseCRUDRepository<User, UserDTO>
+    {
+        public UserRepository(BaseUnitOfWork manager)
+            : base(manager)
+        {
+        }
 
-		public UserDTO GetUserByLogin(string login, string password)
-		{
-			return Single(FunctionConverter.CreateFSharpFunc((Expression<Func<User, bool>>)(t => t.Login == login && t.Password == password)));
-		}
+        public UserDTO GetUserByLogin(string login, string password)
+        {
+            return Single(FunctionConverter.CreateFSharpFunc<User, bool>(t => t.Login == login && t.Password == password));
+        }
 
-		public IEnumerable<UserDTO> ListFull()
-		{
-			var users = All();
+        public IEnumerable<UserDTO> ListFull()
+        {
+            var users = All();
 
-			return DTOBuilder().BuildFullList(users);
-		}
-	}
+            return DTOBuilder().BuildFullList(users);
+        }
+    }
 }
