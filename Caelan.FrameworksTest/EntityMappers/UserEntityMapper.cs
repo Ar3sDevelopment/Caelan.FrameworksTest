9e8e979e419781f7c30491d465009354eb7bb810
Matteo Bruni <Ar3sHack@gmail.com>
2014-12-23 08:56:43 +0100

    Updated packages and fixed what's broken

--- Caelan.FrameworksTest/EntityMappers/UserEntityMapper.cs
+++ Caelan.FrameworksTest/EntityMappers/UserEntityMapper.cs
@@ -1,41 +1,27 @@
 ﻿using System.Linq;
-using Caelan.Frameworks.Common.Interfaces;
+using Caelan.Frameworks.Common.Classes;
 using Caelan.FrameworksTest.Classes;
 using Caelan.FrameworksTest.Models;
-using Microsoft.FSharp.Core;
 
 namespace Caelan.FrameworksTest.EntityMappers
 {
-    public class UserEntityMapper : IMapper<UserDTO, User>
-    {
-        public User Map(UserDTO source)
-        {
-            var dest = new User();
-            var destRef = new FSharpRef<User>(dest);
+	public class UserEntityMapper : DefaultMapper<UserDTO, User>
+	{
+		public override void Map(UserDTO source, ref User destination)
+		{
+			destination.Id = source.Id;
+			destination.Login = source.Login;
+			destination.Password = source.Password;
 
-            Map(source, destRef);
-
-            return destRef.Value;
-        }
-
-        public void Map(UserDTO source, FSharpRef<User> destination)
-        {
-			if (destination.Value == null)
-				destination.Value = new User();
-
-            destination.Value.Id = source.Id;
-            destination.Value.Login = source.Login;
-            destination.Value.Password = source.Password;
-
-            if (source.UserRoles != null)
-            {
-                destination.Value.UserRoles = source.UserRoles.Select(t => new UserRole
-                {
-                    Id = t.Id,
-                    IdUser = t.IdUser,
-                    IdRole = t.IdRole
-                }).ToList();
-            }
-        }
-    }
+			if (source.UserRoles != null)
+			{
+				destination.UserRoles = source.UserRoles.Select(t => new UserRole
+				{
+					Id = t.Id,
+					IdUser = t.IdUser,
+					IdRole = t.IdRole
+				}).ToList();
+			}
+		}
+	}
 }
--- Caelan.FrameworksTestFS/Mappers.fs
+++ Caelan.FrameworksTestFS/Mappers.fs
@@ -1,55 +1,34 @@
 ﻿namespace Caelan.FrameworksTestsFS.EntityMappers
 
-open Caelan.Frameworks.Common.Interfaces
 open Caelan.Frameworks.BIZ.Classes
 open Caelan.FrameworksTestsFS.Models
 open Caelan.FrameworksTestsFS.Classes
 open System.Linq
+open Caelan.Frameworks.Common.Classes
 
 type UserEntityMapper() = 
-    
-    interface IMapper<UserDTO, User> with
-        
-        member this.Map(source : UserDTO) = 
-            let dest = User()
-            let destRef = ref dest
-            this.Map(source, destRef)
-            !destRef
-        
-        member __.Map(source : UserDTO, destination : User ref) = 
-            if !destination = null then destination := User()
-            destination := User(Id = source.Id, Login = source.Login, Password = source.Password)
-            (!destination).UserRoles <- (source.UserRoles |> Seq.map (fun t -> UserRole(IDRole = t.IDRole))).ToList()
-    
-    member this.Map(source : UserDTO) = (this :> IMapper<UserDTO, User>).Map(source)
-    member this.Map(source : UserDTO, destination : User ref) = 
-        (this :> IMapper<UserDTO, User>).Map(source, destination)
+    inherit DefaultMapper<UserDTO, User>()
+    override __.Map(source : UserDTO, destination : User byref) = 
+        destination.Id <- source.Id
+        destination.Login <- source.Login
+        destination.Password <- source.Password
+        destination.UserRoles <- (source.UserRoles |> Seq.map (fun t -> UserRole(IDRole = t.IDRole))).ToList()
 namespace Caelan.FrameworksTestsFS.DTOMappers
 
 open Caelan.Frameworks.Common.Interfaces
 open Caelan.FrameworksTestsFS.Models
 open Caelan.FrameworksTestsFS.Classes
+open Caelan.Frameworks.Common.Classes
 
 type UserDTOMapper() = 
-    
-    interface IMapper<User, UserDTO> with
-        
-        member this.Map(source : User) = 
-            let dest = UserDTO()
-            let destRef = ref dest
-            this.Map(source, destRef)
-            !destRef
-        
-        member __.Map(source : User, destination : UserDTO ref) = 
-            if !destination = null then destination := UserDTO()
-            destination := UserDTO(Id = source.Id, Login = source.Login, Password = source.Password)
-            (!destination).UserRoles <- source.UserRoles 
-                                        |> Seq.map 
-                                               (fun t -> 
-                                               UserRoleDTO
-                                                   (Id = t.Id, IDUser = t.IDUser, IDRole = t.IDRole, 
-                                                    Role = RoleDTO(Id = t.Role.Id, Description = t.Role.Description)))
-    
-    member this.Map(source : User) = (this :> IMapper<User, UserDTO>).Map(source)
-    member this.Map(source : User, destination : UserDTO ref) = 
-        (this :> IMapper<User, UserDTO>).Map(source, destination)
+    inherit DefaultMapper<User, UserDTO>()
+    override __.Map(source : User, destination : UserDTO byref) = 
+        destination.Id <- source.Id
+        destination.Login <- source.Login
+        destination.Password <- source.Password
+        destination.UserRoles <- source.UserRoles 
+                                 |> Seq.map 
+                                        (fun t -> 
+                                        UserRoleDTO
+                                            (Id = t.Id, IDUser = t.IDUser, IDRole = t.IDRole, 
+                                             Role = RoleDTO(Id = t.Role.Id, Description = t.Role.Description)))
